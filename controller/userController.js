51418d53da5240b868a34a067eafec0a5a926f76
kowais915 <kowais915@gmail.com>
2023-07-04 02:01:34 +0500

    :sparkles: Successfully created a token and sent it back to the client

--- controller/userController.js
+++ controller/userController.js
@@ -1,15 +1,28 @@
+const jwt = require('jsonwebtoken')
+const express = require('express');
+require("dotenv").config();
 
 const {
    User
 } = require('../models/UserModel');
+
+// function for creating a token
+
+const createToken = (_id)=>{
+    return jwt.sign({_id: _id}, process.env.SECRET, {expiresIn: '2d'}); 
+}
+
 
 const signUp= async (req ,res )=>{
     const {email, password} = req.body;
    
     try{
         console.log(email);
+       
         const user = await User.signup(email, password);
-        res.status(200).json({email, user});
+         // creating a new token
+         const token = createToken(user._id);
+        res.status(200).json({email, token});
 
     }catch(err){
         res.status(400).json({mssg: err.message});
